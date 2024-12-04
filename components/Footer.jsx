c6339e88bf28c5409c384974a902b30045f9ba99
Ayanoo <gagan270903@gmail.com>
2024-12-04 22:33:09 +0530

    Fixed footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -50,17 +50,31 @@ const Footer = () => {
             <div className='w-full flex justify-start' >
 
                 <div className='flex sm:flex-row flex-col justify-between items-start flex-1 text-white gap-16 sm:gap-0' >
-                    <div className='flex flex-col flex-[0.6]' >
-                        <h3 className='small-heading mb-8' >Subscribe to our newsletter to stay in touch with the latest.</h3>
+                    <div className='flex flex-col-reverse gap-10 sm:flex-col flex-[0.6]' >
 
-                        <Input className="footer_input" placeholder="Your email address" type="text" />
+                        <div className='flex flex-col' >
+                            <h3 className='small-heading mb-8' >Subscribe to our newsletter to stay in touch with the latest.</h3>
+                            <Input className="footer_input" placeholder="Your email address" type="text" />
+                        </div>
+
+                        <div>
+                            <h3 className='small-heading text-white' > Follow Us Here: </h3>
+
+                            <div className='flex gap-6 mt-2' >
+                                {SocialIcons.map((social, index) =>
+                                    <Link href="#" key={index} className='rounded-full text-white border-2 border-white p-2 sm:p-4 hover:bg-white-1 hover:text-black-2 transition-all duration-200' >
+                                        {social.icon}
+                                    </Link>
+                                )}
+                            </div>
+                        </div>
                     </div>
 
                     <div>
                         <ul className='flex flex-col gap-6' >
-                            {Options.map((option , index) =>
-                                <li className='font-medium text-[1.25rem] sm:text-[2rem]'key={index} >
-                                    <Link href="#" className='text-left text-grey-2' > <CustomButton text={option.title}/> </Link>
+                            {Options.map((option, index) =>
+                                <li className='font-medium text-[1.25rem] sm:text-[2rem]' key={index} >
+                                    <Link href="#" className='text-left text-grey-2' > <CustomButton text={option.title} /> </Link>
                                 </li>)}
                         </ul>
                     </div>
@@ -79,17 +93,6 @@ const Footer = () => {
                 <div></div>
             </div>
 
-            <div>
-                <h3 className='small-heading text-white' > Follow Us Here: </h3>
-
-                <div className='flex gap-6 mt-2' >
-                    {SocialIcons.map((social,index) =>
-                        <Link href="#" key={index} className='rounded-full text-white border-2 border-white p-2 sm:p-4 hover:bg-white-1 hover:text-black-2 transition-all duration-200' >
-                            {social.icon}
-                        </Link>
-                    )}
-                </div>
-            </div>
         </footer>
     )
 }
